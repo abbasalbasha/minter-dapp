3036e6e6dc3b93f0d0a967a551545526121fbe0e
goldcryptominer <goldcryptominer@gmail.com>
2022-06-01 17:28:21 +0300

    contract address updated

--- backend/src/config.js
+++ backend/src/config.js
@@ -81,7 +81,7 @@ const PREREVEAL_TOKEN_URI = null; // only update if you want to manually set the
 const PRESALE_WHITELISTED_ADDRESSES = []; // only update if you want to manually set the whitelisted addresses
 
 // ** OPTIONAL **
-let CONTRACT_ADDRESS = "0xF6d5bF090f7eeCf015411Bc7e0CcFa290060B74E"; // If you want to manually include it
+let CONTRACT_ADDRESS = "0x55c3b95502080D31149694407B37B521cd397257"; // If you want to manually include it
 
 // Generic Metadata is optional if you want to reveal your NFTs
 const GENERIC = true; // Set to true if you want to upload generic metas and reveal the real NFTs in the future
